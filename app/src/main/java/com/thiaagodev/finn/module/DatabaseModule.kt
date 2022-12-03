b5ae511f7508f5120747fffa9292b73cd809fca0
Thiago Ferreira <thiaago.dev@gmail.com>
2022-12-03 18:43:07 -0300

    singleton database provider

--- app/src/main/java/com/thiaagodev/finn/module/DatabaseModule.kt
+++ app/src/main/java/com/thiaagodev/finn/module/DatabaseModule.kt
@@ -9,17 +9,20 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
 class DatabaseModule {
 
     @Provides
+    @Singleton
     fun provideAccountDAO(appDatabase: FinnDatabase): AccountDAO {
         return appDatabase.accountDAO()
     }
 
     @Provides
+    @Singleton
     fun provideAppDatabase(@ApplicationContext context: Context): FinnDatabase {
         return Room.databaseBuilder(context, FinnDatabase::class.java, "finnDB")
             .build()
